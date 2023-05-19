25199ec6af78a5b0c4e68f58a24b21adb58570ef
mikan5kiro <133861370+mikan5kiro@users.noreply.github.com>
2023-05-19 13:52:40 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/cat.jpeg') {
+      myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+      myImage.setAttribute('src', 'images/cat.jpeg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName(){
+    let myName =  prompt("input your name!");
+    if(!myName){
+        setUserName();
+    }else{
+        localStorage.setItem('name',myName);
+        myHeading.textContent = '真是太进步啦,' + myName;
+    }
+    
+    
+}
+if(!localStorage.getItem('name')){
+    setUserName();
+}
+else{
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = '真是太进步啦，' + storedName;
+}
+myButton.onclick = function(){
+    setUserName();
+}
